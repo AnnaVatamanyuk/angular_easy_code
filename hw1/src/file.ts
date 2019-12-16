eea7626f8318253f359211eb90f75dedfc4703ec
soulr1ver <dovgal_maxim@mail.ru>
2019-12-17 01:03:12 +0200

    Small changes.

--- hw1/src/file.ts
+++ hw1/src/file.ts
@@ -1,10 +1,9 @@
 //1. реализовать метод, принимающий строку и возвращающий является ли она палиндромом
 
-function isPalindromeString(str: string) {
-    let i: number;
+function isPalindromeString(str: string): boolean {
     if (!str)
         return false;
-    for (i = 0; i < str.length / 2; i++) {
+    for (let i = 0; i < str.length / 2; i++) {
         if (str.charAt(i) !== str.charAt(str.length - i - 1))
             return false;
     }
@@ -17,19 +16,24 @@ console.log(isPalindromeString('assa'));
 // fn(2) => [1,1]
 // fn(4) => [1,1,2,3]
 
-function fibonacciArray(fibonacciNumber: number) {
-    let i: number;
+function fibonacciArray(fibonacciNumber: number): Array<number> {
     let fibonacci: Array<number> = [];
-    if (fibonacciNumber > 0){
-       for (i = 0; i < fibonacciNumber; i++){
-           if (fibonacci.length < 2){
-               fibonacci.push(1);
-           } else if (fibonacci[i - 1] + fibonacci[i - 2] <= fibonacciNumber){
-               fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
-           }
-       }
-       return fibonacci;
+    if (fibonacciNumber <= 0) return fibonacci;
+    for (;;) {
+        let {length} = fibonacci;
+        if (length < 2){
+            fibonacci.push(1);
+            continue;
+        }
+        
+        let nextValue = fibonacci[length - 1] + fibonacci[length - 2];
+        if (nextValue < fibonacciNumber){
+            fibonacci.push(nextValue);
+        } else {
+            break;
+        }
     }
+    return fibonacci;
 }
 
 console.log(fibonacciArray(10));
